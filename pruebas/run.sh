9eff3b36f9673421dff16711214d83e975c8342d
Diego Miguel Lozano <dml1001@alu.ubu.es>
2018-11-21 21:13:06 +0100

    Tareas a, b, d & mejora del script

--- pruebas/run.sh
+++ pruebas/run.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
+cd ../fuente/src
 if [ "$1" = "-s" ]; # skip compilation
 then
-	java -cp . analizadorCSS fichero # execute without compiling
+	java -cp . analizadorCSS ../../pruebas/fichero # execute without compiling
 else
-	cd .. && cp ./fuente/src/analizadorCSS.jj ./pruebas && \
-	cd ./pruebas && javacc analizadorCSS.jj && javac *.java && java -cp . analizadorCSS fichero
+	javacc analizadorCSS.jj && javac *.java && java -cp . analizadorCSS ../../pruebas/fichero
 fi
