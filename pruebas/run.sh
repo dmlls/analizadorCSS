a803818ff74c63e6f34781fec1343203ca7b58e0
Diego Miguel Lozano <dml1001@alu.ubu.es>
2018-12-15 13:44:34 +0100

    Correcciones generales
    Comentarios arreglados y mejora del código y del script.

--- pruebas/run.sh
+++ pruebas/run.sh
@@ -1,9 +1,42 @@
 #! /bin/bash
 
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+# prints help
+function printHelp {
+	echo -e "\n${bold}./run.sh${normal} [OPCIÓN] <FICHERO>\n"
+	echo -e "${bold}OPCIONES:"
+	echo -e "\t-s, --skip${normal}"
+	echo -e "\t\tejecutar sin recompilar"
+	echo -e "${bold}\n\t-h, --help${normal}"
+	echo -e "\t\tobtener ayuda\n"
+}
+
+# prints error message when argument is missing
+function missingArgError {
+	echo -e "\n${bold}ERROR${normal}: Debe especificar el fichero a analizar.\n\n-"
+	printHelp
+}
+
 cd ../fuente/src
-if [ "$1" = "-s" ]; # skip compilation
+
+if [ "$1" = "-h" -o "$1" = "--help" ]; # show help
 then
-	java -cp . analizadorCSS ../../pruebas/fichero # execute without compiling
+	printHelp
+elif [ "$1" = "-s" -o "$1" = "--skip" ]; # skip compilation
+then
+	if [ "$2" = "" ];
+	then
+		missingArgError
+	else
+		java -cp . analizadorCSS ../../pruebas/"$2" # execute without compiling
+	fi
 else
-	javacc analizadorCSS.jj && javac *.java && java -cp . analizadorCSS ../../pruebas/fichero
+	if [ "$1" = "" ];
+	then
+		missingArgError
+	else
+		javacc analizadorCSS.jj && javac *.java && java -cp . analizadorCSS ../../pruebas/"$1"
+	fi
 fi
